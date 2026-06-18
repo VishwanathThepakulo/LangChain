6e322f1c991088b947c0bdc1b5393b5420646b78
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-06-18 22:17:05 +0530

    Implementing dynamic configuration models in langchain

--- models.py
+++ models.py
@@ -6,17 +6,43 @@ load_dotenv()
 
 api_key = os.getenv('GROQ_API_KEY')
 
-model = init_chat_model(
-    "llama-3.3-70b-versatile",
-    model_provider='groq',
+# model = init_chat_model(
+#     "llama-3.3-70b-versatile",
+#     model_provider='groq',
+#     api_key = api_key,
+#     temperature=0.7,
+#     timeout=30,
+#     max_tokens=100,
+#     max_retries=6,
+# )
+
+
+configurable_model = init_chat_model(
+    model = "llama-3.3-70b-versatile",
+    model_provider="groq",
     api_key = api_key,
-    temperature=0.7,
-    timeout=30,
-    max_tokens=100,
-    max_retries=6,
+    max_tokens = 200,
+    configurable_fields=("model", "model_provider", "temperature")
+)
+print("=================Response 1====================")
+response1 = configurable_model.invoke("why do parrot talk?")
+print(response1.content)
+
+print("=================Response 2====================")
+
+model2 = configurable_model.with_config(
+    configurable={
+        'model':"openai/gpt-oss-120b", 
+        'model_provider':'groq',
+        'api_key':api_key,
+        'temperature':0.5,                  
+        'max_tokens':200,
+        'max_retries':4,
+    }
 )
 
-response = model.invoke("why do parrot talk?")
-print(response.content)
+
+response2 = model2.invoke("what is the capital of sun")
+print(response2.content)
 
 
