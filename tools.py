61b0207465ba6c049c5a5e080e150d0cb588167e
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-06-23 21:58:31 +0530

    Implementing  tools

--- tools.py
+++ tools.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import logging
 
+
 load_dotenv()
 
 
@@ -59,7 +60,7 @@ def java_queries(query):
 
 
 
-from langchain_community.tools import DuckDuckGoSearchRun
+from langchain_community.tools import DuckDuckGoSearchRun, ShellTool
 
 @tool(description="Answer the queries related to web search")
 def websearch(query):
@@ -67,12 +68,48 @@ def websearch(query):
     results = search_tool.invoke(query)
     print(results)
 
+@tool(description="This tool is used to excute the shell(Terminal)")
+def shell(query):
+    shell_tool = ShellTool()
+    result = shell_tool.invoke(query)
+    print(result)
+
+import requests
+@tool(description="answer related to weather data(input is only city name)")
+def get_weather(city_name):
+    api_key = os.getenv('WEATHER_APIKEY')
+    base_url = "http://api.openweathermap.org/data/2.5/weather"
+
+    params = {
+            'q': city_name,
+            'appid': api_key,
+            'units': 'metric' 
+        }
+    try:
+        response = requests.get(base_url, params=params)
+        data = response.json()
+
+        if response.status_code==200:
+            main = data['main']
+            weather = data['weather'][0]
+            
+            print(f"--- Weather in {data['name']}, {data['sys']['country']} ---")
+            print(f"Temperature: {main['temp']}°C")
+            print(f"Feels Like : {main['feels_like']}°C")
+            print(f"Humidity   : {main['humidity']}%")
+            print(f"Condition  : {weather['description'].capitalize()}")
+        else:
+            print(f"Error: {data.get('message', 'Unable to fetch data.')}")
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")     
+
 
 
 
 def tool_calling():
-    model_with_tools = model.bind_tools([python_queries, java_queries, websearch])
-    ai_message = model_with_tools.invoke("who is modi")
+    model_with_tools = model.bind_tools([python_queries, java_queries, websearch, shell, get_weather])
+    ai_message = model_with_tools.invoke("climate in hyderabad")
     print("Tool Calls Needed:", ai_message.tool_calls)     
     for tool_call in ai_message.tool_calls:
         if tool_call["name"] == "python_queries":
@@ -87,12 +124,14 @@ def tool_calling():
             result = websearch.invoke(
                 tool_call["args"]
             )
+        elif tool_call["name"] == "shell":
+            result = shell.invoke(
+                tool_call["args"]
+            )
+        elif tool_call["name"] == "get_weather":
+            result = get_weather.invoke(
+                tool_call["args"]
+            )
 
 tool_calling()
 
-# from langchain_community.tools import DuckDuckGoSearchRun
-
-# search_tool = DuckDuckGoSearchRun()
-
-# results = search_tool.invoke('Time in hyderabad india now')
-# print(results)
