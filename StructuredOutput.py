341b09ad37525aa2b8bba9e6ab08a161c84e0353
VishwanathThepakulo <vishwanaththepakulo@gmail.com>
2026-07-01 22:47:10 +0530

    structured output using create agent method

--- StructuredOutput.py
+++ StructuredOutput.py
@@ -1,25 +1,69 @@
+# from langchain.chat_models import init_chat_model
+# from dotenv import load_dotenv
+# import os
+# from pydantic import BaseModel
+
+# load_dotenv()
+
+# class Person(BaseModel):
+#     name: str
+#     age: str
+    
+    
+# api_key = os.getenv("GROQ_API_KEY")
+# if not api_key:
+#     raise Exception("Api key not found")
+# model = init_chat_model(
+#     model='meta-llama/llama-4-scout-17b-16e-instruct',
+#     model_provider='groq',
+#     api_key=api_key
+# )
+    
+# structured_model = model.with_structured_output(Person)
+
+# result = structured_model.invoke("John is 30 years old")
+# print(result.name)
+# print(type(result))
+# results = result.model_dump()
+# print(results['age'])
+# print(type(results))
+
+
+from pydantic import BaseModel, Field
+from langchain.agents import create_agent
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
-import os
-from pydantic import BaseModel
+import os 
 
 load_dotenv()
 
-class Person(BaseModel):
-    name: str
-    age: str
-    
-    
-api_key = os.getenv("GROQ_API_KEY")
+api_key = os.getenv('GROQ_API_KEY')
+
 if not api_key:
     raise Exception("Api key not found")
+
+
+class ContactInfo(BaseModel):
+    name: str = Field(description="The name of the person")
+    email: str = Field(description="The email address of the person")
+    phone: str = Field(description="The phone number of the person")
+
 model = init_chat_model(
-    model='meta-llama/llama-4-scout-17b-16e-instruct',
-    model_provider='groq',
+    model="meta-llama/llama-4-scout-17b-16e-instruct",
+    model_provider="groq",
     api_key=api_key
 )
-    
-structured_model = model.with_structured_output(Person)
 
-result = structured_model.invoke("John is 30 years old")
-print(result)
+
+agent = create_agent(
+    model=model,
+    response_format=ContactInfo
+)
+
+
+result = agent.invoke({
+    "messages": [{"role": "user", "content": "Extract contact info from: John Doe, john@example.com, (555) 123-4567"}]
+})
+
+print(result["structured_response"])
+
